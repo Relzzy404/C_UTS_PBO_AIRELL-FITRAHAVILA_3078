fe48c3fc10b4746ccefd4dc9c0e6dfbb2d1fbf5d
Relzzy404 <relzxyz@gmail.com>
2025-04-25 14:35:16 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace perpustakaan_rz
+{
+    class Book
+    {
+        public int id;
+        public string judul;
+        public string penulis;
+        public int tahunterbit;
+        public string status;
+    }
+
+    class Perpustakaan
+    {
+        public string nama;
+        public string alamat;
+
+        public void TambahBuku(Book)
+        {
+            koleksi.Add(Book);
+            Console.WriteLine("Buku ditambah.");
+        }
+
+        public void TampilkanBuku()
+        {
+            if (koleksi.Count == 0)
+            {
+                Console.WriteLine("Belum ada buku.");
+                return;
+            }
+
+           
